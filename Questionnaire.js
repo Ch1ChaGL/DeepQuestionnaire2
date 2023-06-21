76cb12af1d8d08126c8b3478da9cbff9d4be7c68
Данил Марков <markov.danil.04@mail.ru>
2023-06-21 03:00:02 +0300

    Реализована возможность нескольких условий, например с and и or, конечно вложенных каких-то приколов нету у меня, но пока так, можно сделать вес условия еще, в целом теперь работает опрос немного

--- Questionnaire.js
+++ Questionnaire.js
@@ -17,8 +17,7 @@ class Questionnaire {
     this.answerQuestion = new Map();
   }
 
-  /**
-   * 
+  /** 
    * @param {block} block - блок вопросов
    */
   async processBlock(block) {
@@ -58,25 +57,75 @@ class Questionnaire {
   }
 
   /**
+   * Только возвращает следующий блок, и итерируется по условиям, логика в приватной функции _CheckConditional
    * @param {PreviousBlock} PreviousBlock - предыдущий блок
    * @param {answerQuestion} answerQuestion - Вопрос-Ответ
    */
   getNextBlock(PreviousBlock, answerQuestion){
     console.log('Предыдущий блок:', PreviousBlock);
     console.log('Вопрос-Ответ:', answerQuestion);
+
     const conditions = PreviousBlock['nextBlock']?.['condition'];
     if(!conditions) return null;
+
+    let nextBlockId = null;
+
     for (const condition of conditions) {
-      const questionId = condition['questionId'];
-      const answer = condition['answer'];
-      if(answerQuestion.get(questionId) !== answer) {
-        return null;
-      }
+      console.log('я тут');
+      nextBlockId = this._CheckConditional(condition, answerQuestion);
+      if(nextBlockId) break;
     }
-    const nextBlockId = PreviousBlock['nextBlock']['blockId'];
-    return this.blocks.find(block=> block['id'] === nextBlockId)
+
+    return this.blocks.find(block => block['id'] === nextBlockId)
   }
 
+  /**
+   * Проверка условия
+   * @param {condition} condition - условие
+   * @param {answerQuestion} answerQuestion - Вопрос-Ответ
+   */
+  //Нужно просмотреть предложенный блок, его условие, и если оно верное, то вернуть следующий блок иначе вернуть null
+  _CheckConditional(conditions, answerQuestion){
+    console.log('Я тут');
+    const {Operator, blockId} = conditions[0];
+    console.log('conditions',conditions);
+    console.log('answerQuestion',answerQuestion);
+    console.log('Operator',Operator);
+    console.log('blockId',blockId);
+    console.log("Operator === 'and'", Operator === 'and');
+    if(Operator === 'and'){
+      console.log('Я в and');
+      console.log('conditions', conditions.length);
+      for (let i = 1; i < conditions.length; i++) {
+        console.log('-------------');
+        const questionId = conditions[i]['questionId'];
+        console.log('questionId',questionId);
+        const answer = conditions[i]['answer'];
+        console.log('answer',answer);
+
+        if(answerQuestion.get(questionId) !== answer) {
+          return null;
+        }
+      }
+
+      return blockId;
+    }
+    else if(Operator === 'or'){
+      let orCondition = false;
+      for (let i = 1; i < conditions.length; i++) {
+        const questionId = conditions[i]['questionId'];
+        const answer = conditions[i]['answer'];
+
+        if(answerQuestion.get(questionId) === answer) {
+          orCondition = true;
+        }
+      }
+
+      if(orCondition) return blockId;
+    }
+    else return null;
+    
+  }
 }
 
 module.exports = Questionnaire;
