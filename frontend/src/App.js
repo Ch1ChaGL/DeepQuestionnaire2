456b1136557fee40d8beb9bf6cf5bfd26496703f
Danil Markov <markov.danil.04@mail.ru>
2023-06-26 22:00:40 +0300

    Может я тупой, или как, но я пытался разобраться с bootstrap, я делал страницу авторизации, и когда я ее почти доделал, я столкнулся с тупой проблемой, почему компонент Row не делает адекватные вещи, он просто берет и вытягивает по всей ширине и все, хотя display flex себя не так ведет

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import AppRouter from "./components/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 import { observer } from "mobx-react-lite";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const App = observer(() => {
   return (
     <div className="App">
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  FloatingLabel,
+  Button,
+} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 function Auth() {
   return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <Card className="p-5" style={{ width: "700px" }}>
+        <h1 className="text-center">Авторизация</h1>
+        <Form className="d-flex flex-column">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="mb-3"
+          >
+            <Form.Control type="email" placeholder="name@example.com" />
+          </FloatingLabel>
+          <FloatingLabel controlId="floatingPassword" label="Password">
+            <Form.Control type="password" placeholder="Password" />
+          </FloatingLabel>
 
-    <Container fluid="sm">
-        <Row>
-            <Col>1</Col>
-            <Col>2</Col>
-        </Row>
+          <Row className="d-flex justify-content-between mt-3">
+            <Col sm={9}>Нет аккаунта? Тогда иди на хуй</Col>
+            <Col sm={3} className="text-end">
+              <Button variant="outline-success">Войти</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Card>
     </Container>
-
-  )
+  );
 }
 
-export default Auth
+export default Auth;
